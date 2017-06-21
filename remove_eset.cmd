8c53c25456f87e075c543f9e107d72d6efa73602
TheEmRoD <TheEmRoD@users.noreply.github.com>
2017-06-21 19:47:39 +0200

    Update remove_eset.cmd

--- remove_eset.cmd
+++ remove_eset.cmd
@@ -1,3 +1,8 @@
 @Echo Off
 @Cls
-for /F "tokens=3" %a in ('reg query "HKLM\SOFTWARE\ESET\ESET Security\CurrentVersion\Info" /V ProductCode^| findstr /ri "REG_SZ"') do msiexec /x %a /qn REBOOT="ReallySuppress" PASSWORD=""
+::Source https://community.spiceworks.com/topic/396652-uninstall-eset-smart-security-via-command-line
+::Thanks to blapajk for it's command line
+:: It parses the registry key "HKLM\SOFTWARE\ESET\ESET Security\CurrentVersion\Info"
+:: Looking For the "ProductCode" REG_SZ Value 
+:: And launch the msiexec uninstall command for this ProductId
+for /F "tokens=3" %%a in ('reg query "HKLM\SOFTWARE\ESET\ESET Security\CurrentVersion\Info" /V ProductCode^| findstr /ri "REG_SZ"') do msiexec /x %%a /qn REBOOT="ReallySuppress" PASSWORD=""
