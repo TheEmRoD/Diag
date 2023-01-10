ce4ac005d1fb7b0f93426a45aa083cfb94f0c9b1
TheEmRoD <TheEmRoD@users.noreply.github.com>
2023-01-10 21:29:17 +0100

    Update Windows Install Date.ps1

--- Windows Install Date.ps1
+++ Windows Install Date.ps1
@@ -1,5 +1,6 @@
-(Get-ChildItem -Path HKLM:\System\Setup\Source* | 
+$test=(Get-ChildItem -Path HKLM:\System\Setup\Source* | 
 ForEach-Object {Get-ItemProperty -Path Registry::$_} | 
 Select-Object ProductName, ReleaseID, CurrentBuild, @{n="Install Date"; e={([DateTime]'1/1/1970').AddSeconds($_.InstallDate)}} | 
 Sort-Object "Install Date" | 
 Select-Object -First 1).'Install Date'
+$test.Date.ToString("F")
