d2c75fdfd597204a6754a97cbbe22b3b1e48eba3
TheEmRoD <TheEmRoD@users.noreply.github.com>
2024-01-26 15:17:45 +0100

    Update AssignTV.cmd

--- AssignTV.cmd
+++ AssignTV.cmd
@@ -1,6 +1,6 @@
 IF EXIST "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" (
 	"C:\Program Files (x86)\TeamViewer\TeamViewer.exe" assignment --id 0001CoABChDotsvQvDAR7p9VBnBdqw7tEigIACAAAgAJAHoWTHXkHrEiUZcvSgGV_4RlyNRxJ6gIzfapiFJ0QZoRGkBaKE8T52wfWCOo6v7lm8e6ASEAvQIe1tMy0-E6il0PcIYXuDh6HDtVdmbP98eDGtwlmBJYk9nAVf8KoaosDsyQIAEQ_pqP0wY=)
 	Echo TV Present : %errorlevel%
-	)ELSE(
+	) ELSE (
 	Echo No TV Present
 )
