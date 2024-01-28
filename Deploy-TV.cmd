24fa4348d072a28b824fc3599da2c536793752c6
TheEmRoD <TheEmRoD@users.noreply.github.com>
2024-01-28 11:26:40 +0100

    Update Deploy-TV.cmd

--- Deploy-TV.cmd
+++ Deploy-TV.cmd
@@ -1,11 +1,27 @@
 ::@echo off
 SETLOCAL
 
-IF EXIST "C:\Program Files\TeamViewer\TeamViewer.exe" GOTO :Install
-IF EXIST "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" GOTO :Install
+IF EXIST "C:\Program Files\TeamViewer\TeamViewer.exe" GOTO :Uninstall
+IF EXIST "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" GOTO :Uninstall
 
 Echo NO TV : Quit.
 GOTO :QUIT
+
+:Uninstall
+tasklist /FI "IMAGENAME eq TeamViewer.exe" 2>NUL | find /I /N "TeamViewer.exe">NUL
+if "%ERRORLEVEL%"=="0" (GOTO :KILL) ELSE (GOTO :REMOVEMSI)
+
+:KILL
+taskkill /f /im TeamViewer.exe
+TIMEOUT 2
+GOTO :REMOVEMSI
+
+:REMOVEMSI
+wmic product where vendor="TeamViewer"
+if not "%errorlevel%"=="0" GOTO :CHECKOS
+for /f "tokens=2 delims==" %%f in ('wmic product Where "vendor like 'TeamViewer'" get IdentifyingNumber /value ^| find "="') do set "id=%%f"
+msiexec.exe /x "%id%" /qn
+GOTO :Install
 
 :Install
 REM *******************************************************************************
@@ -23,22 +39,23 @@ IF NOT EXIST %SystemRoot%\system32\curl.exe bitsadmin.exe /transfer "TV" https:/
 powershell Expand-Archive -Path """%~dp0TeamViewer_Host.zip""" -DestinationPath """%~dp0"" -force"
 msiexec.exe /passive /norestart /i  "%~dp0Host\TeamViewer_Host.msi" /qn DESKTOPSHORTCUTS=0 CUSTOMCONFIGID=6vqtzpn APITOKEN=22841762-iM9NxwMlx1b0IDp5fQRI
 
-PING localhost -n 15 >NUL
+TIMEOUT 5
 
 
 IF EXIST "C:\Program Files\TeamViewer\TeamViewer.exe" "C:\Program Files\TeamViewer\TeamViewer.exe" assignment --id 0001CoABChDotsvQvDAR7p9VBnBdqw7tEigIACAAAgAJAHoWTHXkHrEiUZcvSgGV_4RlyNRxJ6gIzfapiFJ0QZoRGkBaKE8T52wfWCOo6v7lm8e6ASEAvQIe1tMy0-E6il0PcIYXuDh6HDtVdmbP98eDGtwlmBJYk9nAVf8KoaosDsyQIAEQ_pqP0wY=
 IF EXIST "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" assignment --id 0001CoABChDotsvQvDAR7p9VBnBdqw7tEigIACAAAgAJAHoWTHXkHrEiUZcvSgGV_4RlyNRxJ6gIzfapiFJ0QZoRGkBaKE8T52wfWCOo6v7lm8e6ASEAvQIe1tMy0-E6il0PcIYXuDh6HDtVdmbP98eDGtwlmBJYk9nAVf8KoaosDsyQIAEQ_pqP0wY=
-PING localhost -n 2 >NUL
+TIMEOUT 2
 
 IF EXIST "C:\Program Files\TeamViewer\TeamViewer.exe" "C:\Program Files\TeamViewer\TeamViewer.exe" assignment --id 0001CoABChDotsvQvDAR7p9VBnBdqw7tEigIACAAAgAJAHoWTHXkHrEiUZcvSgGV_4RlyNRxJ6gIzfapiFJ0QZoRGkBaKE8T52wfWCOo6v7lm8e6ASEAvQIe1tMy0-E6il0PcIYXuDh6HDtVdmbP98eDGtwlmBJYk9nAVf8KoaosDsyQIAEQ_pqP0wY=
 IF EXIST "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" assignment --id 0001CoABChDotsvQvDAR7p9VBnBdqw7tEigIACAAAgAJAHoWTHXkHrEiUZcvSgGV_4RlyNRxJ6gIzfapiFJ0QZoRGkBaKE8T52wfWCOo6v7lm8e6ASEAvQIe1tMy0-E6il0PcIYXuDh6HDtVdmbP98eDGtwlmBJYk9nAVf8KoaosDsyQIAEQ_pqP0wY=
-PING localhost -n 2 >NUL
+TIMEOUT 2
 
 IF EXIST "%~dp0TeamViewer_Host.zip" DEL /Q /F "%~dp0TeamViewer_Host.zip"
 IF EXIST "%~dp0Host" rd /Q /S "%~dp0\Host"
 IF EXIST "%~dp0Full" rd /Q /S "%~dp0\Full"
 
-PING localhost -n 6 >NUL
+TIMEOUT 5
+
 IF EXIST "C:\Program Files\TeamViewer\TeamViewer.exe" "C:\Program Files\TeamViewer\TeamViewer.exe" assignment --id 0001CoABChDotsvQvDAR7p9VBnBdqw7tEigIACAAAgAJAHoWTHXkHrEiUZcvSgGV_4RlyNRxJ6gIzfapiFJ0QZoRGkBaKE8T52wfWCOo6v7lm8e6ASEAvQIe1tMy0-E6il0PcIYXuDh6HDtVdmbP98eDGtwlmBJYk9nAVf8KoaosDsyQIAEQ_pqP0wY=
 IF EXIST "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" assignment --id 0001CoABChDotsvQvDAR7p9VBnBdqw7tEigIACAAAgAJAHoWTHXkHrEiUZcvSgGV_4RlyNRxJ6gIzfapiFJ0QZoRGkBaKE8T52wfWCOo6v7lm8e6ASEAvQIe1tMy0-E6il0PcIYXuDh6HDtVdmbP98eDGtwlmBJYk9nAVf8KoaosDsyQIAEQ_pqP0wY=
 
